118534cceea4d51dd9a42f845c4635f72a7d4b50
Roy Scheffers <r.scheffers@gmail.com>
2017-02-22 14:52:39 -0500

    initial style for home featured blog posts

--- uno/homefeatured-post.php
+++ uno/homefeatured-post.php
@@ -24,40 +24,46 @@ $settings = array(
 				'comments' => 'both'
 				);
 $settings = woo_get_dynamic_values( $settings );
-$title_before = '<h1 class="title entry-title">';
-$title_after = '</h1>';
-if ( ! is_single() ) {
-$title_before = '<h2 class="title entry-title">';
-$title_after = '</h2>';
-$title_before = $title_before . '<a href="' . esc_url( get_permalink( get_the_ID() ) ) . '" rel="bookmark" title="' . the_title_attribute( array( 'echo' => 0 ) ) . '">';
-$title_after = '</a>' . $title_after;
-}
-$page_link_args = apply_filters( 'woothemes_pagelinks_args', array( 'before' => '<div class="page-link">' . __( 'Pages:', 'woothemes' ), 'after' => '</div>' ) );
-woo_post_before();
 ?>
-<article <?php post_class(); ?>>
-<?php
-woo_post_inside_before();
-if ( 'content' != $settings['post_content']) // && ! is_singular() )
-	woo_image( 'width=' . esc_attr( $settings['thumb_w'] ) . '&height=' . esc_attr( $settings['thumb_h'] ) . '&class=thumbnail ' . esc_attr( $settings['thumb_align'] ) );
-?>
-	<header>
-	<?php the_title( $title_before, $title_after ); ?>
-	</header>
-<?php
-//woo_post_meta();
-?>
-	<section class="entry">
-<?php
-if ( 'content' == $settings['post_content'] || is_single() ) { the_content( __( 'Continue Reading &rarr;', 'woothemes' ) ); } else { the_excerpt(); }
-if ( 'content' == $settings['post_content'] || is_singular() ) wp_link_pages( $page_link_args );
-?>
-	</section><!-- /.entry -->
-	<div class="fix"></div>
-<?php
-woo_post_inside_after();
-?>
-</article><!-- /.post -->
+<div class="post-box">
+	<?php
+	$title_before = '<h1 class="title entry-title">';
+	$title_after = '</h1>';
+	if ( ! is_single() ) {
+	$title_before = '<h2 class="title entry-title">';
+	$title_after = '</h2>';
+	$title_before = $title_before . '<a href="' . esc_url( get_permalink( get_the_ID() ) ) . '" rel="bookmark" title="' . the_title_attribute( array( 'echo' => 0 ) ) . '">';
+	$title_after = '</a>' . $title_after;
+	}
+	$page_link_args = apply_filters( 'woothemes_pagelinks_args', array( 'before' => '<div class="page-link">' . __( 'Pages:', 'woothemes' ), 'after' => '</div>' ) );
+	woo_post_before();
+	?>
+
+	<article <?php post_class(); ?>>
+	<?php
+	woo_post_inside_before();
+	if ( 'content' != $settings['post_content']) // && ! is_singular() )
+		woo_image( 'width=' . esc_attr( $settings['thumb_w'] ) . '&height=' . esc_attr( $settings['thumb_h'] ) . '&class=thumbnail ' . esc_attr( $settings['thumb_align'] ) );
+	?>
+		<header>
+		<?php the_title( $title_before, $title_after ); ?>
+		</header>
+	<?php the_date(); ?> posted by <?php echo '<a href="' . get_site_url(null,'/about') . '">' ?>
+			<?php the_author(); ?>
+		</a>
+		<section class="entry">
+	<?php
+	// if ( 'content' == $settings['post_content'] || is_single() ) { the_content( __( 'Continue Reading &rarr;', 'woothemes' ) ); } else { the_excerpt(); }
+	// if ( 'content' == $settings['post_content'] || is_singular() ) wp_link_pages( $page_link_args );
+	?>
+		</section><!-- /.entry -->
+		<div class="fix"></div>
+	<?php
+	woo_post_inside_after();
+	?>
+	</article><!-- /.post -->
+
+</div>
 <?php
 woo_post_after();
 $comm = $settings['comments'];
