1fedda06db60fd23ad3c99ee7e9c0b0b740edf22
Roy Scheffers <r.scheffers@gmail.com>
2017-05-05 12:26:30 -0400

    create podcast template page

---
+++ uno/template-podcast-main.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Template Name: Podcast Main Page
+ *
+ * Here we setup all logic and HTML that is required for the index template, used as both the homepage
+ * and as a fallback template, if a more appropriate template file doesn't exist for a specific context.
+ *
+ * @package WooFramework
+ * @subpackage Template
+ */
+
+get_header(); ?>
+
+    <!-- <div id="content"> -->
+
+    	<?php woo_main_before(); ?>
+
+        <!-- #content Starts -->
+    <?php woo_content_before(); ?>
+    <div id="content" class="col-full">
+      <div id="main-sidebar-container">
+          <!-- #main Starts -->
+          <?php woo_main_before(); ?>
+          <section id="main">
+
+            <?php woo_loop_before();
+
+            if (have_posts()) { $count = 0;
+              while (have_posts()) { the_post(); $count++;
+                woo_get_template_part( 'content', 'page' ); // Get the page content template file, contextually.
+              }
+            }
+            ?>
+    <?php
+
+    $query = new WP_Query( 'cat=108&posts_per_page=-1' );
+
+    if ( $query->have_posts() ) {
+      while ( $query->have_posts() ) { $query->the_post();
+        // woo_get_template_part(  'content-podcast', 'post' );
+        woo_get_template_part( 'content-podcast', get_post_type() );
+
+      }
+    } else {
+      get_template_part( 'content', 'noposts' );
+    }
+    ?>
+
+  </div>
+    <?php woo_loop_after(); ?>
+      </section><!-- /#main -->
+      <?php woo_main_after(); ?>
+
+      <?php get_sidebar(); ?>
+
+    </div><!-- /#main-sidebar-container -->
+
+      <?php get_sidebar( 'alt' ); ?>
+      </div><!-- /#content -->
+
+		<?php woo_main_after(); ?>
+
+  </div><!-- /.content -->
+
+  <?php
+  $link_pregnancy = get_site_url() . '/conscious-pregnancy-empowering-birth';
+  $img_pregnancy = get_site_url() . '/wp-content/uploads/2017/04/conscious-pregnancy-650wide.jpg';
+
+  $link_marriage = get_site_url() . '/getting-married-consciously';
+  $img_marriage = get_site_url() . '/wp-content/uploads/2017/03/getting-married-consciously-BeyondIDo.Julie_.jpg';
+  ?>
+  <div class="col-full archives" style="width: 100%">
+    <h1 class="title entry-title">Radio Shows</h1>
+
+    <div class="podcast-radioshows-wrapper">
+      <a class="podcast-radioshow" href=<?php echo $link_pregnancy; ?>>
+        <img src=<?php echo $img_pregnancy; ?> alt="Conscious Pregnancy" />
+      </a>
+      <a class="podcast-radioshow" href=<?php echo $link_marriage; ?>>
+        <img src=<?php echo $img_marriage; ?> alt="Getting Married Consciously" />
+      </a>
+    </div>
+
+    <h1 class="title entry-title">What listeners are saying</h1>
+    <?php do_action( 'woothemes_testimonials', array( 'limit' => 2, 'id' => '3638, 4175', 'size' => 100, 'per_row' => 2, ) ); ?>
+  </div>
+
+<?php get_footer(); ?>
