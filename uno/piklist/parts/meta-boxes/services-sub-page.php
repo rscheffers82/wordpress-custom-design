bab57e8fea362b64b3c315a1c761a7512d854e49
Roy Scheffers <r.scheffers@gmail.com>
2017-10-12 00:24:41 -0400

    project cleanup piklist & services sub page template

--- uno/piklist/parts/meta-boxes/services-sub-page.php
+++ uno/piklist/parts/meta-boxes/services-sub-page.php
@@ -5,46 +5,6 @@ Post Type: page
 Template: template-services-sub-page, template-services-sub-page-dev
 */
 
-// piklist('field', array(
-//     'type' => 'text'
-//     ,'field' => 'demo_text'
-//     ,'label' => 'Text Box'
-//     ,'description' => 'Field Description'
-//     ,'value' => 'Default text'
-//     ,'help' => 'This is help text.'
-//     ,'attributes' => array(
-//       'class' => 'text'
-//     )
-//   ));
-//
-// piklist('field', array(
-//     'type' => 'select'
-//     ,'field' => 'demo_select'
-//     ,'label' => 'Select Box'
-//     ,'description' => 'Choose from the drop-down.'
-//     ,'help' => 'This is help text.'
-//     ,'attributes' => array(
-//       'class' => 'text'
-//     )
-//     ,'choices' => array(
-//       'option1' => 'Option 1'
-//       ,'option2' => 'Option 2'
-//       ,'option3' => 'Option 3'
-//     )
-//   ));
-//
-// piklist('field', array(
-//     'type' => 'colorpicker'
-//     ,'field' => 'demo_colorpicker'
-//     ,'label' => 'Choose a color'
-//     ,'value' => '#aee029'
-//     ,'description' => 'Click in the box to select a color.'
-//     ,'help' => 'This is help text.'
-//     ,'attributes' => array(
-//       'class' => 'text'
-//     )
-//   ));
-
   piklist('field', array(
       'type' => 'group'
       ,'field' => 'intuition-services'
@@ -107,15 +67,6 @@ Template: template-services-sub-page, template-services-sub-page-dev
           ,'columns' => 12
           ,'attributes' => array(
             'placeholder' => 'in USD'
-          )
-        )
-        ,array(
-          'type' => 'text'
-          ,'field' => 'button'
-          ,'label' => 'button'
-          ,'columns' => 12
-          ,'attributes' => array(
-            'placeholder' => 'Paste redirect URL here...'
           )
         )
       )
--- uno/template-services-sub-page-dev.php
+++ uno/template-services-sub-page-dev.php
@@ -14,14 +14,12 @@ get_header();
 
 function display_button($name, $price, $img_url) {
   echo '<form action="/confirm-booking" method="post">';
-  echo '<input value="Book here" type="submit" class="btn green-full rounded grow">';
-  echo '<input type="hidden" name="package-name" value="' . $name . '">';
-  echo '<input type="hidden" name="package-price" value="' . $price . '">';
-  echo '<input type="hidden" name="package-img-url" value="' . $img_url . '">';
+    echo '<input value="Book here" type="submit" class="btn green-full rounded grow">';
+    echo '<input type="hidden" name="package-name" value="' . $name . '">';
+    echo '<input type="hidden" name="package-price" value="' . $price . '">';
+    echo '<input type="hidden" name="package-img-url" value="' . $img_url . '">';
   echo '</form>';
 }
-
-
 ?>
 <!-- Displays Custom Header image -->
 <?php
@@ -54,23 +52,18 @@ if($wearelive !== "") { ?>
 
   			<?php $get_post_meta = get_post_meta($post->ID, 'intuition-services', false);
   			foreach($get_post_meta[0] as $item) { ?>
-              <?php
-              // helper vars
-              $img_url = wp_get_attachment_url( $item['image'][0] );
-              ?>
+              <?php $img_url = wp_get_attachment_url( $item['image'][0] ); ?>
               <div class="col">
                   <div class="box img-responsive" style="background: url(<?php echo $img_url; ?>)">
                     <a href="http://juliecusmariu.com/julie-cusmariu-life-coaching">
                       <div class="wrapper single">
                           <?php echo $item['title']; ?>
-                          <!-- <div class="underline">When someone is in your corner</div> -->
                       </div>
                     </a>
                   </div>
                   <div class="details">
                     <div class="description"><?php echo wpautop($item['description']); ?></div>
                     <div class="price"><?php echo $item['price']; ?>,-</div>
-                    <!-- <a class="btn green-full grow rounded" href="">Book here</a> -->
                     <?php display_button( $item['title'], $item['price'], $img_url ); ?>
                   </div>
               </div>
