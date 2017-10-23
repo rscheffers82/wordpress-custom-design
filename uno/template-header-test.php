550e37a1c5c9a620b09bb82913a9f3c67df251da
Roy Scheffers <r.scheffers@gmail.com>
2017-10-22 21:50:01 -0500

    initial bugfix for long titles on mobile not showing correctly

---
+++ uno/template-header-test.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Template Name: Header test
+ *
+ * The contact form page template displays the a
+ * simple contact form in your website's content area.
+ *
+ * @package WooFramework
+ * @subpackage Template
+ */
+
+
+get_header();
+?>
+<!-- Displays Custom Header image -->
+<?php
+$wearelive = chi_get_header_image_url();
+if($wearelive !== "") { ?>
+		<section class="home-section custom-header" style="position: relative; background-image:url('<?php chi_display_header(); ?>')">
+      <header><h1 class='title title-entry title-positioning'><?php the_title(); ?></h1></header>
+		</section>
+<?php } ?>
+
+    <!-- #content Starts -->
+	<?php woo_content_before(); ?>
+    <div id="content" class="col-full">
+
+      	<div id="main-sidebar-container">
+
+            <!-- #main Starts -->
+            <?php woo_main_before(); ?>
+            <section id="main">
+<?php
+	woo_loop_before();
+
+	if (have_posts()) { $count = 0;
+		while (have_posts()) { the_post(); $count++;
+			woo_get_template_part( 'content', 'page-title-fix' ); // Get the page content template file, contextually.
+		}
+	}
+
+	woo_loop_after();
+?>
+            </section><!-- /#main -->
+            <?php woo_main_after(); ?>
+
+            <?php get_sidebar(); ?>
+
+		</div><!-- /#main-sidebar-container -->
+
+		<?php get_sidebar( 'alt' ); ?>
+
+    </div><!-- /#content -->
+	<?php woo_content_after(); ?>
+
+<?php get_footer(); ?>
