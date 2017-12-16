c70693087acb440d6bab3498b80f95430c0fdc4b
Roy Scheffers <r.scheffers@gmail.com>
2017-12-16 22:25:56 +0100

    fix apple icon missing on podcast page

--- uno/template-podcast-main.php
+++ uno/template-podcast-main.php
@@ -152,7 +152,7 @@
            </div>
            <div class="podcast-subscribe">
              <h1 class="title" style="font-size: 2em !important;margin-top: .75rem;">Subscribe</h1>
-             <a href="https://itunes.apple.com/us/podcast/heart-beat-internet-radio/id310513252?mt=2" class="btn soft-white rounded grow" target="_blank">  iTunes</a><br>
+             <a href="https://itunes.apple.com/us/podcast/heart-beat-internet-radio/id310513252?mt=2" class="btn soft-white rounded grow" target="_blank"><i class="fa fa-apple"></i> iTunes</a><br>
              <a class="btn soft-white rounded grow" onclick="newsletterFocus()"><i class="fa fa-envelope"></i>  Podcast news</a><br>
              <a href="#listen-to-archives" class="btn soft-white rounded grow podcast podcast-full podcast-full-keep">Listen to archives</a><br>
            </div>
