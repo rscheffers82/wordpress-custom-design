c7d71d55c4f79238f331f84b2560a84c9895d50e
Roy Scheffers <r.scheffers@gmail.com>
2018-02-16 21:33:42 +0100

    add smooth scrolling to call to action buttons

--- uno/js/custom.js
+++ uno/js/custom.js
@@ -3,4 +3,22 @@ jQuery(document).ready(function($){
   $('.testimonials .url a').attr('target','_blank');
 
   $("select#cat").after("<span class='downarrow'></span>");
+
+  $("#podcast-cta, #listen-to-archives, #join-chat").on('click', function(event) {
+    event.preventDefault();   // Prevent default anchor click behavior
+    var hash = this.hash;     // Store hash
+    console.log('in click');
+
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 1500, function(){
+      // Add hash (#) to URL when done scrolling (default click behavior)
+      window.location.hash = hash;
+      console.log('in click, in animate');
+
+    });
+  });
+  console.log('outside click');
 });
